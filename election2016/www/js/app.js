69a459da80b0cd3ed8b810cc8eafaf3b300d6f89
Aditya Gupta <adityag@gmail.com>
2016-06-29 16:26:59 -0700

    Started working on poll

--- election2016/www/js/app.js
+++ election2016/www/js/app.js
@@ -1,5 +1,26 @@
 var election2016 = angular.module('starter', ['ionic', 'firebase']);
 
 election2016.config(function($ionicConfigProvider) {
-  $ionicConfigProvider.navBar.alignTitle('center');
+	$ionicConfigProvider.navBar.alignTitle('center');
+});
+
+election2016.factory("Votes", function($firebaseArray) {
+	var votesRef = new Firebase("https://election2016-efeae.firebaseio.com/votes");
+	return $firebaseArray(votesRef);
+})
+
+election2016.controller("ListCtrl", function($scope, Votes) {
+	$scope.votes = Votes;
+	$scope.voteTrump = function() {
+		$scope.votes.$add({
+			"ip": //lol this isnt done yet skrub
+			"vote": Trump
+		});
+	};
+	$scope.voteClinton = function() {
+		$scope.votes.$add({
+			"ip": //lol this isnt done yet skrub
+			"vote": Clinton
+		});
+	};
 });
