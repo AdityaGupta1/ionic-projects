db911879bc07c52e0de406e36d7594dd3862444b
Aditya Gupta <adityag@gmail.com>
2016-06-29 22:55:20 -0700

    Added IP-finding capabilities, database entries now contain IPs

--- election2016/www/js/app.js
+++ election2016/www/js/app.js
@@ -9,18 +9,40 @@ election2016.factory("Votes", function($firebaseArray) {
 	return $firebaseArray(votesRef);
 })
 
-election2016.controller("ListCtrl", function($scope, Votes) {
+election2016.controller("ListCtrl", function($scope, Votes, $http) {
+	$scope.getIP = function() {
+		var json = 'http://ipv4.myexternalip.com/json';
+		$http.get(json).
+		success(function(data, status, headers, config) {
+			IP = JSON.stringify(data);
+			IP = IP.substring(7, IP.length - 2);
+			console.log(IP);
+		}).
+		error(function(data, status, headers, config) {
+		});
+	}
+
 	$scope.votes = Votes;
+
+	var IP = $scope.getIP();
+
 	$scope.voteTrump = function() {
+		if (IP == 'temp') {
+			$scope.getIP();
+		}
 		$scope.votes.$add({
-			"ip": //lol this isnt done yet skrub
-			"vote": Trump
+			"ip": IP,
+			"vote": 'Trump'
 		});
 	};
+
 	$scope.voteClinton = function() {
+		if (IP == 'temp') {
+			$scope.getIP();
+		}
 		$scope.votes.$add({
-			"ip": //lol this isnt done yet skrub
-			"vote": Clinton
+			"ip": IP,
+			"vote": 'Clinton'
 		});
 	};
 });
